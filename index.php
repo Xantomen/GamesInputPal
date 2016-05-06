767478db2946c56fcb048dabdaf92c1cc74b6bc3
Xantomen <xantomen@gmail.com>
2016-05-06 18:18:41 +0200

    Defined Game Template Object structure for database and committing first iteration of index.php

--- index.php
+++ index.php
@@ -21,7 +21,39 @@
   </head>
   <body>
   	
-  
+  	<?php echo '<p>Hello World</p>'; ?> 
+  	<!--<?php
+		$q = intval($_GET['q']);
+		
+		$con = mysqli_connect('localhost','peter','abc123','my_db');
+		if (!$con) {
+		    die('Could not connect: ' . mysqli_error($con));
+		}
+		
+		/*mysqli_select_db($con,"ajax_demo");
+		$sql="SELECT * FROM user WHERE id = '".$q."'";
+		$result = mysqli_query($con,$sql);
+		
+		echo "<table>
+		<tr>
+		<th>Firstname</th>
+		<th>Lastname</th>
+		<th>Age</th>
+		<th>Hometown</th>
+		<th>Job</th>
+		</tr>";
+		while($row = mysqli_fetch_array($result)) {
+		    echo "<tr>";
+		    echo "<td>" . $row['FirstName'] . "</td>";
+		    echo "<td>" . $row['LastName'] . "</td>";
+		    echo "<td>" . $row['Age'] . "</td>";
+		    echo "<td>" . $row['Hometown'] . "</td>";
+		    echo "<td>" . $row['Job'] . "</td>";
+		    echo "</tr>";
+		}*/
+		echo "LALALALA";
+		//mysqli_close($con);
+		?> -->
   	
   	
   	<div id="container_all">
